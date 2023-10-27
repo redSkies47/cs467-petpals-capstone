f2fc0166aace5f0ac5868421f4c59b842541fbc7
Shukie Li <59763257+lishukie6588@users.noreply.github.com>
2023-10-27 23:17:14 +0700

    admin_landing.py: - took out testing resolution - removed module import for now

--- kivy_app_components/admin_landing.py
+++ kivy_app_components/admin_landing.py
@@ -2,13 +2,9 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen, ScreenManager
-from admin_update_news import AdminUpdateNewsScreen
-
-
 class MainApp(MDApp):
 
     def build(self):
-        Window.size = (295, 620)
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "LightBlue"
         # Builder.load_file('admin_update_news.kv')
