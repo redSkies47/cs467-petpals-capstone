bf0fbf1675697f49fbf3f5ddf22a0d0bd4add30b
Shukie Li <59763257+lishukie6588@users.noreply.github.com>
2023-10-27 20:24:02 +0700

    admin_landing.py - updated filepath for kv file

--- kivy_app_components/admin_landing.py
+++ kivy_app_components/admin_landing.py
@@ -4,6 +4,7 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen, ScreenManager
 from admin_update_news import AdminUpdateNewsScreen
 
+
 class MainApp(MDApp):
 
     def build(self):
@@ -11,23 +12,25 @@ class MainApp(MDApp):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "LightBlue"
         # Builder.load_file('admin_update_news.kv')
-        return Builder.load_file('admin_landing.kv')
-    
+        return Builder.load_file('../kv_design_language/admin_landing.kv')
+
+
 class AdminLandingScreen(Screen):
 
-    #TODO: backend variables for metadata
+    # TODO: backend variables for metadata
 
     def toUpdateNews(self):
         print("Update News")
-        #TODO: navigate to admin side - update news page
+        # TODO: navigate to admin side - update news page
 
     def toAddEditAnimal(self):
         print("Add/Edit Animal")
-        #TODO: navigate to admin side - add/edit animal page
+        # TODO: navigate to admin side - add/edit animal page
 
     def toDeleteAnimal(self):
         print("Delete Animal")
-        #TODO: navigate to admin side - delete animal page
+        # TODO: navigate to admin side - delete animal page
+
 
 if __name__ == '__main__':
     MainApp().run()
