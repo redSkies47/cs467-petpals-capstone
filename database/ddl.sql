247a874885b72b1545384b8b3f2c93abf22ed211
Jonathan Chan <91590206+redSkies47@users.noreply.github.com>
2023-10-27 11:00:51 -0700

    Updated ddl with sample data

--- database/ddl.sql
+++ database/ddl.sql
@@ -218,6 +218,88 @@ VALUES
 ('administrative');
 
 -- -----------------------------------------------------------------------------
+-- Insert sample data into tables
+-- -----------------------------------------------------------------------------
+
+-- Insert new Animals
+INSERT INTO `Animals` (`id_availability`, `id_species`, `id_breed`, `name`,
+`birth_date`, `id_gender`, `size`,
+`summary`,
+`date_created`)
+VALUES
+(1, 3, 36, 'Cleocatra',
+'2019-04-20', 2, 12,
+'Cleocatra is a sweet independent persian cat. She keeps herself very busy but is very sweet and open to some pampering!',
+'2023-10-26'),
+(1, 3, 35, 'Safari',
+'2021-04-02', 4, 8,
+'Looking for a social cat? Look no further than the social Safari! She gets along well with children and other pets.',
+'2023-10-26'),
+(2, 3, 46, 'Tango',
+'2023-01-04', 3, 6,
+'Anywhere is a stage for the singing Tango who is otherwise silently sleeping. Seeks out people but doesn''t quite get along with other animals.',
+'2023-10-26'),
+(1, 2, 18, 'Curls',
+'2018-12-03', 2, 33,
+'Curls came from another home that could no longer care for her. She gets along well with kids and is looking for a great carer who can groom her well.',
+'2023-10-26'),
+(3, 2, 7, 'Luna',
+'2023-06-29', 1, 21,
+'Puppy Luna has been getting lots of training. He is very playful with kids and other animals.',
+'2023-10-26'),
+(4, 2, 15, 'Snowy',
+'2023-02-10', 3, 32,
+'Snowy had to part ways from his liter recently. Expect lots of sassy vocals from him! Can be a bit too excited on walks.',
+'2023-10-26'),
+(1, 1, 1, 'Coco',
+'2021-06-20', 2, 5,
+'Persevering Coco went through a house fire where her caretakers had to let her go. She is very social and can speak several phrases.',
+'2023-10-26');
+
+-- Insert new Animal_Dispositions
+INSERT INTO `Animal_Dispositions` (`id_animal`, `id_disposition`)
+VALUES
+(1, 5),
+(1, 6),
+(2, 1),
+(2, 2),
+(2, 5),
+(3, 7),
+(3, 8),
+(4, 2),
+(4, 6),
+(5, 1),
+(5, 2),
+(6, 3),
+(6, 8),
+(7, 4),
+(7, 8);
+
+-- Insert new Accounts
+INSERT INTO `Accounts` (`id_credential`, `email`, `password`, `name`)
+VALUES
+(2, 'director@petrefuge.org', 'L4ve@nimal$', 'Pet Refuge'),
+(1, 'eSharp@gmail.com', 'dogSearch4', 'Enrique Sharp'),
+(1, 'tashHop@icloud.com', 'sw33Tc@ts', 'Tasha Hopkins');
+
+-- Insert new Shelters
+INSERT INTO `Shelters` (`name`, `address`, `phone_number`,
+`opening_hour`, `closing_hour`)
+VALUES
+('Pet Refuge', '2273 Bel Meadow Drive, Bellevue Oregon',
+'909-410-5017', 9, 17);
+
+-- Insert new News
+INSERT INTO `News` (`date`, `title`, `body`)
+VALUES
+('2023-10-26', 'No Fees November',
+'This upcoming month of November, all fees will be waived. The adoptions fees for cats, dogs, and other animals at Pet Refuge will come down to $0! This is brought to the Bellevue community by the generous fundraising efforts of the Forest Rangers of the North. Home a nice furry companion before the holidays! Take the steps to learn about a lifestyle compatibility for interested animals. Stop on by at Pet Refuge today!'),
+('2023-10-27', 'Found Kittens?',
+'Before contacting the Pet Refuge shelter, learn about some steps to take when finding kittens in your community. 1. Is the mother cat still around? Figure out if the mother cat is seen around the kittens for a period of around half a day. If the mother cat is no longer around, make an assessment on the condition of the kittens... Follow the link to learn more... https://rroll.to/hjme3D'),
+('2023-10-28', 'Winter 5k Run Fundraiser',
+'This December 10 at Bellevue Community Center, there is a local 5k Winter Run. Join Pet Refuge in the 5k Winter Run as a volunteer or donor. Volunteers can join the 5k Run as representative runners. Another opportunity is to be present in the tent providing information and other goodies to the community about Pet Refuge. Or support our fundraising efforts by donating on behalf of our runners! To learn more visit our 5k Winter Run''s event page.');
+
+-- -----------------------------------------------------------------------------
 -- Re-enable commits and foreign key checks
 -- -----------------------------------------------------------------------------
 SET FOREIGN_KEY_CHECKS = 1;
