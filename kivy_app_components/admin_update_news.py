d1655da80cc5640349786ea9676a76d130f017e9
Shukie Li <59763257+lishukie6588@users.noreply.github.com>
2023-10-28 03:54:00 +0700

    1) admin_landing.py 2) admin_landing.kv 3) admin_update_news.py 4) admin_update_news.kv NOTES: - py files & ky files all modular - still need troubleshooting to have them work in seperate ky & py dirs

--- kivy_app_components/admin_update_news.py
+++ kivy_app_components/admin_update_news.py
@@ -2,28 +2,22 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen, ScreenManager
-from admin_update_news import admin_update_news
 
-class admin_landing(MDApp):
 
-    def build(self):
-        Window.size = (295, 620)
-        self.theme_cls.theme_style = "Light"
-        self.theme_cls.primary_palette = "LightBlue"
-        return Builder.load_file('admin_landing.kv')
-    
+
+class admin_update_news(Screen):
+
+    # TODO: backend variables for metadata
+
     def toUpdateNews(self):
         print("Update News")
         #TODO: navigate to admin side - update news page
 
     def toAddAnimal(self):
+        # Builder.load_file('admin_update_news.kv')
         print("Add Animal")
         #TODO: navigate to admin side - add/edit animal page
 
     def toEditDeleteAnimal(self):
         print("Edit/Delete Animal")
         #TODO: navigate to admin side - delete animal page
-
-
-if __name__ == '__main__':
-    admin_landing().run()
