927b0de7cd67f1b10dea77fab3de688f0e0f9d32
Samantha Lin <linsam@engr.orst.edu>
2023-11-01 20:04:38 -0700

    Fixed file imports | Renamed files to match documentation

--- kivy_app_components/user_front.py
+++ kivy_app_components/user_front.py
@@ -1,14 +1,15 @@
+import kivy
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.core.window import Window
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
-from kivy.uix.popup import Popup
+# from kivy.core.window import Window
+# from kivy.uix.button import Button
+# from kivy.uix.textinput import TextInput
+# from kivy.uix.gridlayout import GridLayout
+# from kivy.uix.widget import Widget
+# from kivy.properties import ObjectProperty
+# from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 
@@ -17,7 +18,7 @@ class LoginScreen(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "LightBlue"
-        return Builder.load_file('user_front.kv')
+        return Builder.load_file('../kv_design_language/user_front.kv')
 
 class LoginWindow(Screen):
     pass
@@ -28,13 +29,13 @@ class CreateAccountWindow(Screen):
 class LandingWindow(Screen):
     def __init__(self, **kwargs):
         super(LandingWindow, self).__init__(**kwargs)
-        self.change_label = Label(text="Changes Submitted!", 
+        self.change_label = Label(text="Changes Submitted!",
                               color = (255/255, 0/255, 0/255, 1),
-                              font_size = 25, 
+                              font_size = 25,
                               pos_hint = {"center_x": 0.5, "center_y": .95})
-        self.delete_label = Label(text="Account Deleted.", 
+        self.delete_label = Label(text="Account Deleted.",
                               color = (255/255, 0/255, 0/255, 1),
-                              font_size = 25, 
+                              font_size = 25,
                               pos_hint = {"center_x": 0.5, "center_y": .95})
     def press_change(self):
         self.add_widget(self.change_label)
@@ -55,7 +56,7 @@ class WindowManager(ScreenManager):
 
 class MainApp(App):
     def build(self):
-        return Builder.load_file('user_front.kv')
+        return Builder.load_file('../kv_design_language/user_front.kv')
 
 if __name__ == '__main__':
     LoginScreen().run()
