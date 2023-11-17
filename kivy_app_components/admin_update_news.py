c980b428ef2fa898f3138b33a3415c1b3be7f377
Shukie Li <59763257+lishukie6588@users.noreply.github.com>
2023-11-18 01:48:45 +0800

    feature branch - browse animals

--- kivy_app_components/admin_update_news.py
+++ kivy_app_components/admin_update_news.py
@@ -17,6 +17,12 @@ DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
 
+# local
+DB_HOST = 'localhost'
+DB_USER = 'shukie'
+DB_PASSWORD = 'password'
+DB_NAME = 'capstone'
+
 
 class admin_update_news(Screen):
 
