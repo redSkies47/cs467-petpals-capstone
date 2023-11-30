581c871ce156db72354c0fa0c144e5b9725d1399
Jonathan Chan <91590206+redSkies47@users.noreply.github.com>
2023-11-29 23:10:42 -0800

    Update admin_update_news.py

--- kivy_app_components/admin_update_news.py
+++ kivy_app_components/admin_update_news.py
@@ -16,8 +16,7 @@ DB_HOST = "classmysql.engr.oregonstate.edu"
 DB_USER = "capstone_2023_petpals"
 DB_PASSWORD = "ph[r2cZ[QXqX9Ag8"
 DB_NAME = "capstone_2023_petpals"
-DB_NAME = os.getenv('DB_NAME')
-TOKEN = os.getenv('TOKEN')
+TOKEN = "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"
 
 
 class admin_update_news(Screen):
